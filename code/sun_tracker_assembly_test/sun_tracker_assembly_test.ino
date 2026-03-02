6cdb9e9ae38b783348b2d78c8a6054efb463b54d
Ueli Niederer <ueli.niederer@niederer-engineering.ch>
2026-03-02 16:40:56 +0100

    Add license and setup instructions.

--- code/sun_tracker_assembly_test/sun_tracker_assembly_test.ino
+++ code/sun_tracker_assembly_test/sun_tracker_assembly_test.ino
@@ -1,6 +1,31 @@
+/*
+ * This file is part of the EduDemoS Sun-Tracker which is
+ * co-funded by the European Union.
+ * Copyright (C) 2025  Gerda Stetter Stiftung - Technik macht Spaß!
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <Arduino.h>
+
+/*  To setup your project:
+    1. Create a copy of configuration.default.cpp
+    2. Name the copy "configuration.cpp"
+    3. Adjust the settings in configuration.cpp according to your needs 
+       (look for @todo-comments) */
 #include "configuration.cpp"
 
+// Include Libraries
 #include <Ticker.h>
 
 #include <MqttClient.h>
